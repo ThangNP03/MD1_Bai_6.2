b3c32db2d2624ce5768da0881334fad8f1bce399
Your Name <2003nguyenphucthang@gmail.com>
2022-12-09 10:04:31 +0700

    MD1_Bai_6.2

---
+++ Luyện tập/java.js
@@ -0,0 +1,105 @@
+// <!--Bài 1 trong luyện tập-->
+//
+// <!--     <h1> mời nhập số bất kỳ</h1>-->
+// <!--     <div id = "output"></div>-->
+// <!--     <div> Mời nhập số A <input type="text" id ="numbera"></div>-->
+// <!--<br>-->
+// <!--     <div> Mời nhập số B <input type="text" id ="numberb"></div>-->
+// <!--<br>-->
+// <!--     <button onclick="checkNumber()">Check Number</button>-->
+//
+// <!--<script>-->
+// <!--    function checkNumber() {-->
+// <!--        let numberA = +document.getElementById('numbera').value-->
+// <!--        let numberB = +document.getElementById('numberb').value-->
+// <!--        difference = numberA / numberB-->
+//
+// <!--        if (numberA % numberB ==  0){-->
+// <!--            document.getElementById('output').innerHTML = numberA + " chia hết cho  " + numberB-->
+// <!--        }-->
+// <!--        else{-->
+// <!--            document.getElementById('output').innerHTML = numberA + " Không chia hết cho " + numberB-->
+// <!--        }-->
+// <!--    }-->
+// <!--</script>-->
+
+// <!--Bài 2 teong phần luyện tập-->
+//
+//
+//     <!--<script>-->
+//     <!--    function check() {-->
+//     <!--        let condition = +document.getElementById('input').value-->
+//
+//     <!--        if (condition >= 16 ){-->
+//     <!--            document.getElementById('output').innerHTML = condition + " tuổi đủ điều kiện để  học cấp 3"-->
+//     <!--        }else {-->
+//     <!--            document.getElementById('output').innerHTML = condition + " tuổi không đủ điềukiện để học cấp 3"-->
+//     <!--        }-->
+//     <!--    }-->
+//     <!--</script>-->
+
+// <!--Bài 3 trong luyện tập-->
+// <!--<script>-->
+// <!--    let value = prompt('Mời nhập vào số bát kì ', '')-->
+// <!--    if (value > 0) {-->
+// <!--        alert('Đâyb là số lớn hơn 0')-->
+// <!--    }-->
+// <!--    else if( value == 0){-->
+// <!--        alert('Đâyn là số bằng 0')-->
+// <!--    }-->
+// <!--    else {-->
+// <!--        alert('Đây là số bé hơn không')-->
+// <!--    }-->
+// <!--</script>-->
+
+// Bài 4 trong phần luyện tập
+// <!--<script>-->
+// <!--    function checkNumber(){-->
+// <!--        let input1 = +document.getElementById('input1').value-->
+// <!--        let input2 = +document.getElementById('input2').value-->
+// <!--        let input3 = +document.getElementById('input3').value-->
+// <!--        let max = input1-->
+// <!--        if (input2 > max ){-->
+// <!--            max = input2-->
+// <!--        } if( input3 > max) {-->
+// <!--            max = input3-->
+// <!--        }-->
+// <!--         if( input1 > max) {-->
+// <!--            max = input1-->
+// <!--        }-->
+// <!--        document.getElementById('output').innerHTML = max + " là số lớn nhất "-->
+// <!--    }-->
+// <!--</script>-->
+
+// Bài 5 trong phần luyện tập
+<!--<script>-->
+<!--    function check() {-->
+<!--        let input1 = +document.getElementById('input1').value-->
+<!--        let input2 = +document.getElementById('input2').value-->
+<!--        result = (input1 + input2) / 2-->
+<!--          if (result >= 8) {-->
+<!--            document.getElementById('output').innerHTML = "Học sinh giỏi"-->
+<!--            }-->
+<!--          else  if (result >= 6.5 ){-->
+<!--              document.getElementById('output').innerHTML = " Học sinh khá "-->
+<!--          }-->
+<!--          else if ( result >= 5 ){-->
+<!--              document.getElementById('output').innerHTML = " học sinh trung bình"-->
+<!--          }-->
+<!--          else {-->
+<!--              document.getElementById('output').innerHTML = " Học sinh kém"-->
+<!--          }-->
+<!--        console.log(result)-->
+<!--    }-->
+// <!--</script>-->
+
+// Bài 6 trong phần luyện tập
+// <script>
+//     function doanhThu() {
+//     let input = +document.getElementById('input').value
+//     let input1 = +document.getElementById('input1').value
+//     result = input * (input1/100)
+//     document.getElementById('output').innerHTML = "Doanh  thu hoa hồng nhận được là " + result
+//     console.log(result)
+// }
+// </script>
